49faf5783b45aaa2ed57fdef971827dc1d10a966
eminbasbayan <emin.basbayan@icloud.com>
2022-10-13 02:13:47 +0300

    product items glide.js entegrasyonu

--- js/glide.js
+++ js/glide.js
@@ -1,4 +1,39 @@
-new Glide(".glide", {
+const config = {
   type: "carousel",
-  perView: 3,
-}).mount();
+  perView: 4,
+  gap: 20,
+  /*   autoplay: 3000, */
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".product-carousel", config).mount();
+
+const config2 = {
+  type: "carousel",
+  perView: 4,
+  gap: 20,
+  /*   autoplay: 3000, */
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".product-carousel2", config2).mount();
