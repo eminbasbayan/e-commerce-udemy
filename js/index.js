3652a2fd92abc6e3824a421d8bf63f78a4e19c4b
eminbasbayan <emin.basbayan@icloud.com>
2022-10-12 20:16:49 +0300

    search modal

--- js/index.js
+++ js/index.js
@@ -22,3 +22,34 @@ document.addEventListener("click", function (event) {
 /* click outside end */
 
 //! home sidebar end
+
+//! search modal start
+const btnOpenSearch = document.querySelector(".search-button");
+const btnCloseSearch = document.getElementById("close-search");
+const modalSearch = document.getElementsByClassName("modal-search");
+const modalSearchWrapper = document.getElementsByClassName("modal-wrapper");
+
+btnOpenSearch.addEventListener("click", function () {
+  modalSearch[0].style.visibility = "visible";
+  modalSearch[0].style.opacity = "1";
+});
+
+btnCloseSearch.addEventListener("click", function () {
+  modalSearch[0].style.visibility = "hidden";
+  modalSearch[0].style.opacity = "0";
+});
+
+/* click outside start */
+document.addEventListener("click", function (e) {
+  if (
+    !e.composedPath().includes(modalSearchWrapper[0]) &&
+    !e.composedPath().includes(btnOpenSearch)
+  ) {
+    modalSearch[0].style.visibility = "hidden";
+    modalSearch[0].style.opacity = "0";
+  }
+});
+
+/* click outside end */
+
+//! search modal end
