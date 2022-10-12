6269c4bc0d54dc89863453a99098d0ca38631266
eminbasbayan <emin.basbayan@icloud.com>
2022-10-12 18:43:54 +0300

    header sidebar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log("hello world!");
+//! home sidebar start
+const btnOpenSidebar = document.querySelector("#btn-menu");
+const sidebar = document.querySelector("#sidebar");
+const btnCloseSidebar = document.querySelector("#close-sidebar");
+btnOpenSidebar.addEventListener("click", function () {
+  sidebar.style.left = "0";
+});
+
+btnCloseSidebar.addEventListener("click", function () {
+  sidebar.style.left = "-100%";
+});
+
+//! home sidebar end
