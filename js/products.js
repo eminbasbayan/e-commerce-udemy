604b377a26331a71d43902d71d7644d6cb5a3814
eminbasbayan <emin.basbayan@icloud.com>
2022-10-13 21:28:58 +0300

    router işlemleri

--- js/products.js
+++ js/products.js
@@ -31,6 +31,18 @@ function addToCart() {
   });
 }
 
+function productRoute() {
+  const productLink = document.getElementsByClassName("product-link");
+  Array.from(productLink).forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      const id = e.target.dataset.id;
+      localStorage.setItem("productId", JSON.stringify(id));
+      window.location.href = "single-product.html";
+    });
+  });
+}
+
 function productsFunc() {
   const productsContainer = document.getElementById("product-list");
 
@@ -75,7 +87,7 @@ function productsFunc() {
           <button>
             <i class="bi bi-heart-fill"></i>
           </button>
-          <a href="#">
+          <a href="#" class="product-link" data-id=${item.id}>
             <i class="bi bi-eye-fill"></i>
           </a>
           <a href="#">
@@ -89,6 +101,7 @@ function productsFunc() {
     addToCart();
   });
   product1();
+  productRoute();
 }
 
 export default productsFunc;
