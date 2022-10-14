39f252461ae473eda8b2c85e59990ff5e1503327
eminbasbayan <emin.basbayan@icloud.com>
2022-10-14 21:19:32 +0300

    modal dialog javascript

--- js/main.js
+++ js/main.js
@@ -20,3 +20,23 @@ cartItems.innerHTML = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart")).length
   : "0";
 //! add cartItems to localStorage end
+
+//! modal dialog start
+const modalDialogDOM = document.querySelector(".modal-dialog");
+const modalContentDOM = document.querySelector(".modal-dialog .modal-content");
+const btnCloseDialog = document.querySelector(".modal-dialog .modal-close");
+
+btnCloseDialog.addEventListener("click", function () {
+  modalDialogDOM.classList.remove("show");
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.composedPath().includes(modalContentDOM)) {
+    modalDialogDOM.classList.remove("show");
+  }
+});
+
+setTimeout(() => {
+  modalDialogDOM.classList.add("show");
+}, 3000);
+//! modal dialog end
