765df392d00e0546af36ec06ed4b8c7709a93051
eminbasbayan <emin.basbayan@icloud.com>
2022-10-14 20:13:51 +0300

    ürün arama

--- js/search.js
+++ js/search.js
@@ -13,10 +13,10 @@ function productRoute() {
   });
 }
 
-function searchFunc(data) {
+function searchFunc(products) {
   const searchWrapperDOM = document.querySelector(".search-results .results");
   let result = "";
-  data.forEach((item) => {
+  products.forEach((item) => {
     result += `
     <a href="#" class="result-item" data-id=${item.id}>
         <img src=${item.img.singleImage} class="search-thumb" alt="">
@@ -30,6 +30,63 @@ function searchFunc(data) {
   });
   searchWrapperDOM.innerHTML = result;
   productRoute();
+
+  const searchInputDOM = document.querySelector(".search-form input");
+  let value = "";
+  let filtered = [];
+  searchInputDOM.addEventListener("input", (e) => {
+    value = e.target.value;
+    value = value.trim().toLowerCase();
+    filtered = products.filter((item) =>
+      item.name.trim().toLowerCase().includes(value)
+    );
+    let result = "";
+    if (filtered.length > 1) {
+      searchWrapperDOM.style.gridTemplateColumns = "1fr 1fr";
+      filtered.forEach((item) => {
+        result += `
+        <a href="#" class="result-item" data-id=${item.id}>
+            <img src=${item.img.singleImage} class="search-thumb" alt="">
+            <div class="search-info">
+                <h4>${item.name}</h4>
+                <span class="search-sku">SKU: PD0016</span>
+                <span class="search-price">$${item.price.newPrice.toFixed(
+                  2
+                )}</span>
+            </div>
+        </a>
+        `;
+      });
+      searchWrapperDOM.innerHTML = result;
+    } else if (filtered.length < 2) {
+      searchWrapperDOM.style.gridTemplateColumns = "1fr";
+      if (filtered == 0) {
+        searchWrapperDOM.innerHTML = `
+        <a href="#" class="result-item" style="justify-content: center">
+        😔Aradığınız Ürün Bulunamadı😔
+        </a>
+        `;
+      } else {
+        filtered.forEach((item) => {
+          result += `
+          <a href="#" class="result-item" data-id=${item.id}>
+              <img src=${item.img.singleImage} class="search-thumb" alt="">
+              <div class="search-info">
+                  <h4>${item.name}</h4>
+                  <span class="search-sku">SKU: PD0016</span>
+                  <span class="search-price">$${item.price.newPrice.toFixed(
+                    2
+                  )}</span>
+              </div>
+          </a>
+          `;
+        });
+        searchWrapperDOM.innerHTML = result;
+      }
+    }
+
+    productRoute();
+  });
 }
 
 export default searchFunc;
